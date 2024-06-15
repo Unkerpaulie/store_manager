b91720593c58b4bf10a94c79927b376eeb43d8f9
Paul Pedro <unkerpaulie@gmail.com>
2024-06-15 16:43:31 -0400

    added apps in settings, added products csv, setting up script

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "store",
+    "customers",
+    "products",
+    "orders",
 ]
 
 MIDDLEWARE = [
